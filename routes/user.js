f604e8b50a7096fb9428628856a8cf97cffc735b
MLa2610GitHub <90907877+MLa2610GitHub@users.noreply.github.com>
2022-09-28 23:04:16 +0200

    Actualisation route user/signUp

--- routes/user.js
+++ routes/user.js
@@ -7,11 +7,14 @@ const router = express.Router();
 //Importation du middleware d'authentification
 const auth = require('../middleware/auth');
 
+//Importation du middleware de vérification du mot de passe
+const verifyPassword = require('../middleware/verifyPassword');
+
 const userController = require('../controllers/user');
 
 //Création des routes user et de leurs middlewares respectifs
 
-router.post("/signUp", userController.signUp);
+router.post("/signUp", verifyPassword, userController.signUp);
 router.post("/login", userController.login);
 
 
