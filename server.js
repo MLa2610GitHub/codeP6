2f414dcfa1e764185597ed28edf51f1624c4eeae
MLa2610GitHub <90907877+MLa2610GitHub@users.noreply.github.com>
2022-09-28 22:49:03 +0200

    Correctif port 3000

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
 const errorHandler = error => {
@@ -46,3 +46,4 @@ server.on("listening", () => {
 });
 
 server.listen(port);
+
