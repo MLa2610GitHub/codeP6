176c615e7d041a303dbf957d51e0a4d8593424ec
MLa2610GitHub <90907877+MLa2610GitHub@users.noreply.github.com>
2022-09-28 17:21:45 +0200

    Révision des messages d'erreur

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,4 @@
-//Importation de multer, qui permet de gérer les fichiers entrants dans les requêtes HTTP
+//Importation de multer qui est un package de gestion de fichiers
 const multer = require('multer');
 
 //Création de la constante dictionnaire de type MIME pour gérer l'extension de fichier appropriée
@@ -11,18 +11,15 @@ const MIME_TYPES = {
 
 //Création d'une constante storage à passer à multer pour lui indiquer où enregistrer les fichiers entrants
 const storage = multer.diskStorage({
-  //la fonction destination indique à multer d'enregistrer les fichiers dans le dossier images
   destination: (req, file, callback) => {
     callback(null, "images");
   },
-  //la fonction filename dit à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
-  //utilisation de la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + "." + extension);
   },
 });
 
-//Exportation de multer en indiquant qu'il gére uniquement les téléchargements de fichiers images
+//Exportation de multer en indiquant qu'il gére uniquement les téléchargements de fichiers image
 module.exports = multer({ storage: storage }).single("image");
